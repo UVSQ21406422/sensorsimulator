6c9919be8b28df97d9965f5341f195ab5945029e
huangchen5555 <huangchen5555@eaaf63d0-ae7f-11de-ae21-b99107b3ee63>
2009-10-23 13:51:31 +0000

    Version 1.31 Test version of state listner

--- trunk/src/userinterfaces/AdvancedSettingsFrame.java
+++ trunk/src/userinterfaces/AdvancedSettingsFrame.java
@@ -29,7 +29,7 @@ public class AdvancedSettingsFrame extends javax.swing.JFrame {
         this.simdriver = simdriver;
         initComponents();
         loadAdvancedPropertyToFrame();
-        this.setBounds(MainFrame.windowwidth / 4, MainFrame.windowheight / 4, MainFrame.windowwidth / 2, MainFrame.windowheight / 2);
+      this.setLocation((int)(MainFrame.windowwidth-this.getSize().getWidth())/2, (int)(MainFrame.windowheight-this.getSize().getHeight())/2);
     }
 
     /** This method is called from within the constructor to
